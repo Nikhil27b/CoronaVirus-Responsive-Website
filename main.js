711f0e84f3f27ee34ff5dc9effa93e0d74176bc8
NIKHIL <nikhilhack27@gmail.com>
2020-05-03 11:26:57 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+fetch('https://api.covid19api.com/summary')
+    .then((apidata) => {
+        return apidata.json();
+        // console.log(apidata);
+    })
+
+    .then((actualdata) => {
+
+            console.log(actualdata);
+            //confirm
+            const mydata = actualdata.Countries[100].TotalConfirmed;
+            document.getElementById('conin').innerHTML = mydata;
+
+
+            //recover
+            const recdata = actualdata.Countries[100].TotalRecovered;
+            document.getElementById('revin').innerHTML = recdata;
+
+            //deaths
+            const detdata = actualdata.Countries[100].TotalDeaths;
+            document.getElementById('detin').innerHTML = detdata;
+
+            //global data 
+
+            const globco = actualdata.Global.TotalConfirmed;
+            document.getElementById('globco').innerHTML = globco;
+
+            //global recover
+            const globro = actualdata.Global.TotalRecovered;
+            document.getElementById('globro').innerHTML = globro;
+            //global deaths
+
+            const globdo = actualdata.Global.TotalDeaths;
+            document.getElementById('globdo').innerHTML = globdo;
+        }
+
+    )
+    .catch((error) => {
+        console.log(`the error: ${error}`);
+    });
